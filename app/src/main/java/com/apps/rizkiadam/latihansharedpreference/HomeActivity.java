1ec36cd1cb0da4e3373749e3a8f64ed60f762a2b
idhamrahadian09 <idhamrahadian09@gmail.com>
2019-05-28 20:55:02 +0700

    Push01

---
+++ app/src/main/java/com/apps/rizkiadam/latihansharedpreference/HomeActivity.java
@@ -0,0 +1,51 @@
+package com.apps.rizkiadam.latihansharedpreference;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.apps.rizkiadam.latihansharedpreferences.utils.Preferences;
+
+/*
+
+Author  : Rizki Adam Kurniawan
+email : rizkiadamunikom@gmail.com
+ig : @rizkiadamkurniawan
+
+ */
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView txtKeluar;
+    private TextView txtName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        declareView();
+        txtKeluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Clear Set Preferences
+                Preferences.setLogout(getBaseContext());
+
+                //Pindah Halaman ke Login
+                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+    private void declareView() {
+        txtKeluar = findViewById(R.id.txt_logout);
+        txtName = findViewById(R.id.txtName);
+
+        txtName.setText(Preferences.getRegisteredUser(getBaseContext()));
+    }
+}
